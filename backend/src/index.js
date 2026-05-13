86a7e4199e25665adcaaee1f6a9c049c40649fe2
parthib <parthib.chakraborty.dev@gmail.com>
2026-05-13 22:21:11 +0530

    fix: correct import typo and improve CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,21 +4,23 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { updateClickCountInDB } from "./workers/update_click_count_db.js";
 import requestLogger from "./middlewares/requestLogger.js";
-import requetIdMiddleware from "./middlewares/requestIdForLogger.js";
+import requestIdMiddleware from "./middlewares/requestIdForLogger.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger-output.json" with { type : "json" };
 
 const pool = await connectDB();
 const app = express();
 
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-    origin:"*",
-    methods: ["GET","POST"],
-    allowedHeaders:["Content-Type"]
-}));
-app.use(requetIdMiddleware);
+
+
+app.use(requestIdMiddleware);
 app.use(requestLogger);
 
 app.use("/api/v0/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
